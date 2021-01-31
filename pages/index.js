7472a379da314fbd36fbee610a6bd60900296bb1
jaypatel0311 <senghanijay11@gmail.com>
2021-01-31 18:45:27 +0530

    quizeeey

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Quize</title>
+        <title>Quizeeey</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
     </div>
