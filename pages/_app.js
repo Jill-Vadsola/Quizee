a622e9acc98dd28019ecf6d1d253f7f80b611800
Shreyansh sheth <shethshreyansh777@gmail.com>
2021-01-31 16:54:28 +0530

    complete setup code

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "next/head";
+import React from "react";
+import Appbar from "../src/components/AppBar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </head>
+      <div>
+        <Appbar></Appbar>
+        <Component {...pageProps} />
+      </div>
+    </React.Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ src/components/AppBar.js
@@ -0,0 +1,50 @@
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import Button from "@material-ui/core/Button";
+import MenuIcon from "@material-ui/icons/MenuOutlined";
+
+/**
+ *
+ *
+ */
+import router from "next/router";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 15,
+  },
+}));
+export default function Appbar() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Quize
+          </Typography>
+          <Button color="inherit" onClick={(e) => router.push("/Join")}>
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
