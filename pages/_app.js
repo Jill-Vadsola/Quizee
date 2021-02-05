ab4362b2375f8fed458fdd3ce3ef50b4595434c9
Jill-Vadsola <vadsolajill@gmail.com>
2021-02-05 18:45:56 +0530

    Head

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,14 @@ import "next/head";
 import React from "react";
 import Appbar from "../src/components/AppBar";
 import "antd/dist/antd.css";
-
+import Head from "next/head"
 function MyApp({ Component, pageProps }) {
   return (
     <React.Fragment>
-      <head></head>
+      <Head>
+        <title>Quizee</title>
+        <link rel="icon"></link>
+      </Head>
       <div>
         <Appbar></Appbar>
         <Component {...pageProps} />
